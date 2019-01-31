19c5f1c00a975638ab6efddd876eb63e40f419e1
Eric Kouakou <erickouakou@usf.edu>
2019-01-31 18:44:04 -0500

    version 2

--- array/Program.cs
+++ array/Program.cs
@@ -6,7 +6,30 @@ namespace array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declaring an array
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+            try
+            {
+
+                foreach (int i in numbers)
+                {
+                    Console.WriteLine("Element Value={0}", +i);
+                }
+                Console.WriteLine("");
+                Console.WriteLine("__________________________________________");
+                Console.WriteLine("Press any key to exit the program");
+                Console.ReadKey(true);
+            }//End of  a try
+
+
+            catch
+            {
+                Console.WriteLine("Please exit now.");
+                Console.ReadKey(true);
+            }
+            
+
+           
         }
     }
 }
